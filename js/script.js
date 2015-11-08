e9cccd09c0394dd367331a2ac03cec14af4a74e7
johnrdixon <johnryandixon@gmail.com>
2015-11-07 19:25:31 -0500

    Added Nav

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
 		    baseX       = $window.height() / docHeight * 100,
 		    topOfOthDiv = $( "#othdiv" ).offset().top;
 
-		bar.css( 'background', '-webkit-linear-gradient(left, #002041 '+ baseX +'%, #a6cca7 '+ baseX +'%)');
+		bar.css( 'background', '-webkit-linear-gradient(left, #002041 '+ baseX +'%, #b2b2b2 '+ baseX +'%)');
 	    bar.hide(); //hide your div initially
 
 	    $window.on( "resize", function( e ){
@@ -24,7 +24,7 @@
 	    $window.on( "scroll", function( e ){
 
 		    var x = $window.scrollTop() / docHeight * 100 + baseX;
-		    bar.css('background', '-webkit-linear-gradient(left, #002041 '+ x +'%, #a6cca7 '+ x +'%)');
+		    bar.css('background', '-webkit-linear-gradient(left, #002041 '+ x +'%, #b2b2b2 '+ x +'%)');
 	        
 	        if( $window.scrollTop() > topOfOthDiv ){ //scrolled past the other div?
 	            bar.show(300); //reached the desired point -- show div
