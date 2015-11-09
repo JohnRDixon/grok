4e61378a450a4da4ce1ab0e25963a9ca5234d405
johnrdixon <johnryandixon@gmail.com>
2015-11-09 16:19:56 -0500

    Hopefully final

--- js/script.js
+++ js/script.js
@@ -10,10 +10,12 @@
 		    $doc        = $( document ),
 		    docHeight   = $doc.height(),
 		    baseX       = $window.height() / docHeight * 100,
-		    topOfOthDiv = $( "#othdiv" ).offset().top;
+		    topOfOthDiv = $( "#othdiv" ).offset().top,
+		    nav			= $( "#nav" );
 
-		bar.css( 'background', '-webkit-linear-gradient(left, #002041 '+ baseX +'%, #b2b2b2 '+ baseX +'%)');
+		bar.css( 'background', '-webkit-linear-gradient(left, #f7941d '+ baseX +'%, #b2b2b2 '+ baseX +'%)');
 	    bar.hide(); //hide your div initially
+	    nav.hide();
 
 	    $window.on( "resize", function( e ){
 
@@ -24,14 +26,34 @@
 	    $window.on( "scroll", function( e ){
 
 		    var x = $window.scrollTop() / docHeight * 100 + baseX;
-		    bar.css('background', '-webkit-linear-gradient(left, #002041 '+ x +'%, #b2b2b2 '+ x +'%)');
+		    bar.css('background', '-webkit-linear-gradient(left, #f7941d '+ x +'%, #b2b2b2 '+ x +'%)');
 	        
 	        if( $window.scrollTop() > topOfOthDiv ){ //scrolled past the other div?
 	            bar.show(300); //reached the desired point -- show div
+	            nav.show(300);
 	        }
 	        else{
 	        	bar.hide(300); //reached the desired point -- show div
+	        	nav.hide(300);
 	        }
 	    });
+
+		$(document).ready(function(){
+			$('a[href^="#"]').on('click',function (e) {
+			    e.preventDefault();
+
+			    var target = this.hash;
+			    var $target = $(target);
+
+			    $('html, body').stop().animate({
+			        'scrollTop': $target.offset().top - 90
+			    }, 900, 'swing', function () {
+			        window.location.hash = target;
+			    });
+			});
+		});
+
 	});
+
+
 })( window, undefined );
